824974b152227bec4d51561442d35dfbaaeb7de3
ZeBolHD <zezebolz@gmail.com>
2024-03-30 16:31:24 +0300

    chore(frontend): added fonts

---
+++ apps/frontend/src/lib/fonts.ts
@@ -0,0 +1,9 @@
+import { Cormorant, Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
+
+const cormorant = Cormorant({
+  subsets: ["latin"]
+});
+
+export { raleway, cormorant };
