4dd76f3591696a3558550e1ec05cf9fe58e1e6d2
ZeBolHD <zezebolz@gmail.com>
2024-03-30 16:30:21 +0300

    style(frontend): removed and added classes

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -14,9 +14,17 @@ const config: Config = {
         "primary-light": "#D9BFB0",
         "secondary-light": "#C0A18F",
         "primary-bg": "#F2F5EA"
+      },
+      container: {
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1160px"
+        }
       }
     }
   },
   plugins: []
-}
-export default config
+};
+export default config;
